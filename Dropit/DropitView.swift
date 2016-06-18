39ae35a056d40fcb5b5663d728d57b9b34bb1ee4
hugofalkman <zilla@hs-macbook-pro.local>
2016-06-18 22:49:11 +0200

    Basic gameView setup

---
+++ Dropit/DropitView.swift
@@ -0,0 +1,13 @@
+//
+//  DropitView.swift
+//  Dropit
+//
+//  Created by H Hugo Falkman on 2016-06-18.
+//  Copyright © 2016 H Hugo Falkman. All rights reserved.
+//
+
+import UIKit
+
+class DropitView: UIView {
+
+ }
---
+++ Dropit/DropitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DropitViewController.swift
+//  Dropit
+//
+//  Created by H Hugo Falkman on 2016-06-18.
+//  Copyright © 2016 H Hugo Falkman. All rights reserved.
+//
+
+import UIKit
+
+class DropitViewController: UIViewController {
+
+    @IBOutlet weak var gameView: DropitView!
+    
+}
