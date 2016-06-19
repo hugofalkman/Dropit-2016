e5a1558d3c6eafc009dab0a18232cafbd0ce3c89
hugofalkman <zilla@hs-macbook-pro.local>
2016-06-20 00:14:11 +0200

    Lecture 14 complete

--- Dropit/DropitViewController.swift
+++ Dropit/DropitViewController.swift
@@ -9,7 +9,29 @@
 import UIKit
 
 class DropitViewController: UIViewController {
-
-    @IBOutlet weak var gameView: DropitView!
     
+    
+    @IBOutlet weak var gameView: DropItView! {
+        didSet {
+            gameView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(addDrop(_:))))
+            gameView.addGestureRecognizer(UIPanGestureRecognizer(target: gameView, action: #selector(DropItView.grabDrop(_:))))
+            gameView.realGravity = true
+        }
+    }
+    
+    func addDrop(recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .Ended {
+            gameView.addDrop()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        gameView.animating = true
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        gameView.animating = false
+    }
 }
